a5e23187dcc8e79e690d083efd9cd0b384f0e544
dodgehealer <dodgehealer@gmail.com>
2024-06-16 14:58:53 +0000

    vid process

--- video-ps/src/index.ts
+++ video-ps/src/index.ts
@@ -1,8 +1,25 @@
 import express  from "express";
 import ffmpeg from "fluent-ffmpeg"
 const app = express();
-const port =3000;
+app.use(express.json());
+app.post("/process-video",(req,rep)=> {
+    const inputFilePath= req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+    if (!inputFilePath || !outputFilePath) {
+        return rep.status(400).send("ERROR !, Missing input or output file path");
+    }
+    ffmpeg(inputFilePath).videoFilters('scale=-1:360')
+    .on("end", ()=>{
+        return rep.status(200).send("SUCCESS !, Video has finished successfully")
+    })
+    .on("error",(err)=>{
+        console.log(`An error has occured ${err.message}`)
+        rep.status(500).send("ERROR !, An error has occured")
+    })
+    .save(outputFilePath)
 
+})
+const port =process.env.PORT||3000;
 app.listen(port, ()=>{
     console.log(`vid-ps app listening on port ${port}`)
 })
